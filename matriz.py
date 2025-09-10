7c001ab9077bbc29f9a91c8150930826351b98ab
lopes00-hue <felipelopes0140@gmail.com>
2025-09-10 09:35:39 -0300

    Add files via upload

---
+++ matriz.py
@@ -0,0 +1,10 @@
+Matriz =[
+    
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+
+]
+
+print(Matriz [1][1])
+# linha 0,coluna 2>3
---
+++ numpy.py
@@ -0,0 +1,3 @@
+import numpy as np
+vetor = np.array ([10,20,30,40])
+print(vetor *2) #20,40,60,80
